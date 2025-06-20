f76186d223e258895d11540bd94b7ad18a4ad703
AshishNith <ranjan.ashish9992@gmail.com>
2025-06-20 09:15:01 +0530

    Uodate

--- Backend/models/User.js
+++ Backend/models/User.js
@@ -6,6 +6,7 @@ const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   phone: { type: String, required: true },
   address: {
+    
     street: String,
     city: String,
     state: String,
